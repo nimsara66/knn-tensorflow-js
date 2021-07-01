248316990cb2cebc667e10c1ac2032538f1ffc4d
Nimsara Fernando <nimsara66@gmail.com>
2021-07-01 16:27:14 +0530

    with only lat and long

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-require('@tensorflow/tfjs-node');
+// require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
+
+function knn(features, labels, predictionPoint, k) {
+    return features
+        .sub(predictionPoint)
+        .pow(2)
+        .sum(1, { keepDims: true })
+        .pow(0.5)
+        .concat(labels, 1)
+        .unstack()
+        .sort((a, b) => {
+            a.slice(0, 1).dataSync()[0] > b.slice(0, 1).dataSync()[0] ? 1: -1;
+        })
+        .slice(0, k)
+        .reduce((acc ,pair) => acc + pair.slice(1, 1).dataSync()[0], 0) / k;
+}
 
 let { features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv', {
     shuffle: true,
@@ -9,5 +24,13 @@ let { features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv'
     labelColumns: ['price']
 });
 
-console.log(testFeatures);
-console.log(testLabels);
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+// testFeatures = tf.tensor(testFeatures);
+// testLabels = tf.tensor(testLabels);
+
+testFeatures.forEach((testPoint, i) => {
+    const result = knn(features, labels, tf.tensor(testPoint), 10);
+    const err = (testLabels[i][0] - result) / testLabels[i][0]
+    console.log('Error', err * 100);
+});
